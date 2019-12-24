a4c4a44575dcc6b4a391e03f449d226a91742d11
heruihong0325 <18655377896@163.com>
2019-12-24 15:35:21 +0800

    新增消息驱动项目

---
+++ src/test/java/com/example/stream/StreamApplicationTests.java
@@ -0,0 +1,40 @@
+package com.example.stream;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.annotation.Output;
+import org.springframework.integration.support.MessageBuilder;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@EnableBinding(value = {StreamApplicationTests.SinkSender.class})
+public class StreamApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+	@Autowired
+	private SinkSender sinkSender;
+
+	@Test
+	public void sinkSenderTester() {
+		sinkSender.output().send(MessageBuilder.withPayload("produce a message ：http://blog.didispace.com").build());
+	}
+
+	public interface SinkSender {
+
+		String OUTPUT = "input";
+
+		@Output(SinkSender.OUTPUT)
+		MessageChannel output();
+
+	}
+
+
+}
